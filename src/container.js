e9447d9d6c0bee8dd74a2fb91d16d3dcfa750c78
scottascott <scottwangg@gmail.com>
2021-12-06 21:54:00 -0500

    Update how to setup

--- src/container.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var awilix_1 = require("awilix");
-var event_service_1 = require("./routes/event/services/event-service");
-var container = (0, awilix_1.createContainer)();
-container.register({
-    eventService: (0, awilix_1.asClass)(event_service_1.EventService)
-});
-exports["default"] = container;
